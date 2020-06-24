9052c48137ddeff9822ad580339cd8124c46dd7d
Charles Lu <kuanminglu0529@gmail.com>
2020-06-24 15:23:01 -0400

    Menu added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,15 +2,43 @@ import React from 'react';
 import Auxiliry from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Auxiliry>
-        <Toolbar/>
-        <main className={classes.LayoutContent}>
-            {props.children}
-        </main>
-    </Auxiliry>
-    
-);
+class Layout extends React.Component{
 
-export default layout;
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showSideDrawer: false
+        }
+    }
+
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer:false});
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+
+    render() {
+        return(
+            <Auxiliry>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer 
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.LayoutContent}>
+                    {this.props.children}
+                </main>
+            </Auxiliry>
+        );
+    }
+        
+}
+
+export default Layout;
